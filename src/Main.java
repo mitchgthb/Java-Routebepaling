de7c69d9a22c5f112a117059bf6401cc4099dace
mitchgthb <mitchelldpalm@hotmail.com>
2023-05-16 11:01:31 +0200

    Nearest neighbor algorithm & DB connection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        City a = new City("Boston", 42.3601, -71.0589);
+        City b = new City("Houston", 29.7604, -95.3698);
+        System.out.println(b.measureDistance(a));
+//        System.out.println(b.measureDistanceTwo(a));
+
     }
 }
